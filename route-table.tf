fac644b71d745c5772159d0ae66f729620d98d8a
sunaina156 <sunainalodha475@gmail.com>
2026-05-11 19:09:13 +0530

    Added route table and subnet association configuration

---
+++ route-table.tf
@@ -0,0 +1,17 @@
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  tags = {
+    Name = "public-route-table"
+  }
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "public_subnet_association" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_route_table.id
+}
